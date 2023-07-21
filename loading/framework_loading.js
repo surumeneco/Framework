c698880038e0638137276ad0ff5a026bc3a12818
surumeneco <surumeneko164@gmail.com>
2023-07-21 11:16:49 +0900

    読み込みファイル追加

---
+++ loading/framework_loading.js
@@ -0,0 +1,9 @@
+document.write('<script src="./framework/color/colors_HEX.js"></script>');
+document.write('<script src="./framework/color/colors_String.js"></script>');
+
+document.write('<script src="./framework/file_loading/json_loading.js"></script>');
+
+document.write('<script src="./framework/phina.js"></script>');
+
+document.write('');
+
